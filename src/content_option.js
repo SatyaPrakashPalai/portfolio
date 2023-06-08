ecea0761a9a282594c134cbc1c2845c2cbbcc4f7
SatyaPrakashPalai <sp8368755@gmail.com>
2023-06-08 18:45:48 +0530

    fix: added the tinderclone link

--- src/content_option.js
+++ src/content_option.js
@@ -1,112 +1,114 @@
 const logotext = "SPP";
 const meta = {
-    title: "Satya Prakash Palai",
-    description: "I’m Satya Prakash student in Material Science Engineering _ Full stack devloper,currently studying at NIT Calicut",
+  title: "Satya Prakash Palai",
+  description:
+    "I’m Satya Prakash student in Material Science Engineering _ Full stack devloper,currently studying at NIT Calicut",
 };
 
 const introdata = {
-    title: "I’m Satya Prakash Palai",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "Material Science student",
-    },
-    description: "Frontend Developer, learning Backend ",
-    your_img_url: "/projects/satya.jpeg",
+  title: "I’m Satya Prakash Palai",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "Material Science student",
+  },
+  description: "Frontend Developer, learning Backend ",
+  your_img_url: "/projects/satya.jpeg",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Experienced frontend developer skilled in responsive website design and interactive feature implementation for optimal user experience, actively learning backend development",
+  title: "abit about my self",
+  aboutme:
+    "Experienced frontend developer skilled in responsive website design and interactive feature implementation for optimal user experience, actively learning backend development",
 };
-const worktimeline = [
+const worktimeline = [];
+
+const skills = [
+  {
+    name: "Python",
+    value: 60,
+  },
+  {
+    name: "Css",
+    value: 75,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Node",
+    value: 85,
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 60,
-    },
-    {
-        name: "Css",
-        value: 75,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Node",
-        value: 85,
-    },
-];
+const services = [];
 
-const services = [
-];
+const dataportfolio = [
+  {
+    img: "/projects/tathva.png",
+    name: "Tathva '22",
+    dev: "Web development",
+    link: "https://www.tathva.org/",
+  },
+  {
+    img: "/projects/googlewow.png",
+    name: "GDSC WoW",
+    dev: "Web development",
 
-const dataportfolio = [{
-        img: "/projects/tathva.png",
-        name:"Tathva '22",
-        dev:"Web development",
-        link: "https://www.tathva.org/",
-    },
-    {
-        img: "/projects/googlewow.png",
-        name:"GDSC WoW",
-        dev:"Web development",
+    link: "https://googlewow.vercel.app/",
+  },
+  {
+    img: "/projects/pre-reg.png",
+    name: "Tathva '22 Registration",
+    dev: "Web development",
 
-        link: "https://googlewow.vercel.app/",
-    },
-    {
-        img: "/projects/pre-reg.png",
-        name:"Tathva '22 Registration",
-        dev:"Web development",
-
-        link: "https://registrations-frontend-aadhavanpl.vercel.app/",
-    },
-    {
-        img: "/projects/tinder-clone.png",
-        name:"Tinder Clone",
-        dev:"Web development",
-        link: "https://tinder-clone-zoh8.vercel.app/signup",
-    },
-    {
-        img: "/projects/goodway.png",
-        name:"Goodway",
-        dev:"Web development",
-        link: "https://goodway-sepia.vercel.app/",
-    },
-   
+    link: "https://registrations-frontend-aadhavanpl.vercel.app/",
+  },
+  {
+    img: "/projects/tinder-clone.png",
+    name: "Tinder Clone",
+    dev: "Web development",
+    link: "https://tinder-frontend-eight.vercel.app/",
+  },
+  {
+    img: "/projects/goodway.png",
+    name: "Goodway",
+    dev: "Web development",
+    link: "https://goodway-sepia.vercel.app/",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "spp8368755@gmail.com",
-    description: "Experienced frontend developer skilled in responsive website design and interactive feature implementation for optimal user experience, actively learning backend development",
-     // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_hdlk7ut",
-    YOUR_TEMPLATE_ID: "template_dti07ap",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "spp8368755@gmail.com",
+  description:
+    "Experienced frontend developer skilled in responsive website design and interactive feature implementation for optimal user experience, actively learning backend development",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_hdlk7ut",
+  YOUR_TEMPLATE_ID: "template_dti07ap",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com/SatyaPrakashPalai",
-    instagram: "https://www.instagram.com/hustlerboy_satya/",
-    linkedin: "https://www.linkedin.com/in/satya-prakash-palai-94b314214/",
-    twitter: "https://twitter.com/SatyaPr97170370",
+  github: "https://github.com/SatyaPrakashPalai",
+  instagram: "https://www.instagram.com/hustlerboy_satya/",
+  linkedin: "https://www.linkedin.com/in/satya-prakash-palai-94b314214/",
+  twitter: "https://twitter.com/SatyaPr97170370",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
